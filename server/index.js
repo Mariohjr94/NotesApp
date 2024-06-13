823b9d52c99c4d28a894a25daa7f5b2828074d98
Mariohjr94 <mariohjr94@gmail.com>
2024-06-12 20:15:40 -0400

    Use body-parser in index.js and configure middleware

--- server/index.js
+++ server/index.js
@@ -38,8 +38,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   "/assets",
   express.static(path.join(__dirname, "../client/public/assets"))
