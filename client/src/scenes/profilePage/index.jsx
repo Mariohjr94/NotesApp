a5f22335e241797b49e3e3ff00d497aa60cf8b5f
Mariohjr94 <mariohjr94@gmail.com>
2024-06-24 13:34:55 -0400

    debugging

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
 
   const getUser = async () => {
     const response = await fetch(
-      `${import.meta.env.VITE_REACT_APP_API_BASE_URL}/${userId}`,
+      `${import.meta.env.VITE_REACT_APP_API_BASE_URL}/users/${userId}`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
