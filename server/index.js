906d3fce17b7b30899c8ebc384c8cf134e6935c4
Mariohjr94 <mariohjr94@gmail.com>
2024-06-25 10:01:44 -0400

    debugging

--- server/index.js
+++ server/index.js
@@ -84,20 +84,17 @@ app.get("/", (req, res) => {
 });
 
 // Catch-all handler to serve React's index.html for any route not handled by API
+// app.get("*", (req, res) => {
+//   const validApiPaths = ["/auth", "/users", "/posts", "/chats", "/messages"];
+//   if (validApiPaths.some((path) => req.originalUrl.startsWith(path))) {
+//     res.status(404).send("Not found");
+//   } else {
+//     res.sendFile(path.join(buildPath, "index.html"));
+//   }
+// });
+
 app.get("*", (req, res) => {
-  const validApiPaths = [
-    "/",
-    "/auth",
-    "/users",
-    "/posts",
-    "/chats",
-    "/messages",
-  ];
-  if (validApiPaths.some((path) => req.originalUrl.startsWith(path))) {
-    res.status(404).send("Not found");
-  } else {
-    res.sendFile(path.join(buildPath, "index.html"));
-  }
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
 app.use((req, res, next) => {
